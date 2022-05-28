570828e4dde33135699ec03a44c679fc803e24ac
Samuel Alene <smlalene@gmail.com>
2022-05-28 15:04:04 -0700

    fixing streamlit

--- web_app/data.py
+++ web_app/data.py
@@ -6,7 +6,7 @@ def write():
     with st.spinner("Loading Data ..."):
         st.title('Data description  ')
         # na_value=['',' ','nan','Nan','NaN','na', '<Na>']
-        train = pd.read_csv('./train.csv', engine='python') #na_values=na_value)
+        train = pd.read_csv('../web_app/train.csv', engine='python') #na_values=na_value)
         store = pd.read_csv('./store.csv', engine= 'python')#na_values=na_value)
         full_train = pd.merge(left = train, right = store, how = 'inner', left_on = 'Store', right_on = 'Store')
         full_train = full_train.set_index('Store')
